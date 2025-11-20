1235439b404d420adc8b953ca9e1cef708e1d4f1
ChoHawon <godhw1018@naver.com>
2025-11-20 17:53:16 +0900

    Title: 배열의 합 계산하기, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251120/배열의 합 계산하기/calculate-sum-of-array.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        int[] a = {3,1,4,5,9};
+
+        int res = a[1] + a[3] + a[4];
+
+        System.out.println(res);
+    }
+}
