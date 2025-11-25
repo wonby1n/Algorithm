d18da3d34684e49aafd66d11a47c4796343cf5fe
ChoHawon <godhw1018@naver.com>
2025-11-25 14:45:19 +0900

    Title: 따옴표 출력, Time: 87ms, Memory: 9MB, Status: Passed - Codetree

---
+++ 251125/따옴표 출력/print-quote.java
@@ -0,0 +1,8 @@
+public class Main {
+    public static void main(String[] args) {
+        // Please write your code here.
+        String str = "He says \"It's a really simple sentence\".";
+
+        System.out.println(str);
+    }
+}
