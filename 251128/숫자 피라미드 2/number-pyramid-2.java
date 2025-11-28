defce4a9536ee5d104bc060c9e678985586d5a87
ChoHawon <godhw1018@naver.com>
2025-11-28 09:17:19 +0900

    Title: 숫자 피라미드 2, Time: 152ms, Memory: 10MB, Status: Passed - Codetree

---
+++ 251128/숫자 피라미드 2/number-pyramid-2.java
@@ -0,0 +1,23 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        // Please write your code here.
+		 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		 StringBuilder sb = new StringBuilder();
+		 StringTokenizer st;
+		 
+		 st = new StringTokenizer(br.readLine());
+		 int N = Integer.parseInt(st.nextToken());
+		 
+		 int num = 1;
+		 for (int i = 0; i < N; i++) {
+			 for (int j = 0; j <= i; j++) {
+				 System.out.print((num) + " ");
+				 num++;
+			 }
+			 System.out.println();
+		 }
+    }
+}
