0c47a9893e343d7f866603f4589a7611596da701
ChoHawon <godhw1018@naver.com>
2025-12-02 17:11:46 +0900

    Title: 좌우로 반복하며 출력, Time: 89ms, Memory: 9MB, Status: Passed - Codetree

--- 251202/좌우로 반복하며 출력/print-in-left-right-reverse.java
+++ 251202/좌우로 반복하며 출력/print-in-left-right-reverse.java
@@ -10,22 +10,23 @@ public class Main {
         st = new StringTokenizer(br.readLine());
         int N = Integer.parseInt(st.nextToken());
 
-        // int cnt = 1;
-        
-        for (int i = 1; i <= N; i++) {
-            System.out.print(i);
-        }
-        System.out.println();
-        for (int i = N; i > 0; i--) {
-            System.out.print(i);
-        }
-        System.out.println();
-        for (int i = 1; i <= N; i++) {
-            System.out.print(i);
-        }
-        System.out.println();
-        for (int i = N; i > 0; i--) {
-            System.out.print(i);
+        // 바깥 for: 줄 수를 N번 반복
+        for (int line = 1; line <= N; line++) {
+
+            // 홀수 줄: 1 ~ N 출력
+            if (line % 2 == 1) {
+                for (int i = 1; i <= N; i++) {
+                    System.out.print(i);
+                }
+            } 
+            // 짝수 줄: N ~ 1 출력
+            else {
+                for (int i = N; i >= 1; i--) {
+                    System.out.print(i);
+                }
+            }
+
+            System.out.println();  // 줄 바꿈
         }
     }
 }
